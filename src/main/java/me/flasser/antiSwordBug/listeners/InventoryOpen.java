4e5e229c835f3db096ed95098954913b3e4aac19
Soren1 <soren@frylund.dk>
2025-09-03 17:11:19 +0200

    Bug fix.

--- src/main/java/me/flasser/antiSwordBug/listeners/InventoryOpen.java
+++ src/main/java/me/flasser/antiSwordBug/listeners/InventoryOpen.java
@@ -1,11 +1,11 @@
 package me.flasser.antiSwordBug.listeners;
 
+import me.flasser.antiSwordBug.AntiSwordBug;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.plugin.Plugin;
 
 import me.flasser.antiSwordBug.utils.FixSwordBug;
 
@@ -13,7 +13,7 @@ public class InventoryOpen implements Listener {
 
     @EventHandler
     public void onInventoryOpen(InventoryOpenEvent e) {
-        Bukkit.getScheduler().runTaskLater((Plugin) this, () -> {
+        Bukkit.getScheduler().runTaskLater(AntiSwordBug.instance, () -> {
             FixSwordBug.fixSwordBug((CraftPlayer) e.getPlayer());
         }, 1);
     }
